ee5f158a754be08fbc9d3090c90b9f70c7b76bed
Rinafahridawati <Rinafahridawti@yahoo.co.id>
2017-11-06 23:38:25 +0800

    memberi inputan dan menyimpannya di library

--- Soal 2.cpp
+++ Soal 2.cpp
@@ -4,4 +4,12 @@ using namespace std;
 int main(){ 
 	char pilih,A ,B ;
 	int al, p, l, t, L ;
+	
+	cout <<"A : Persegi Panjang"<<endl;
+	cout <<"B : Segitiga"<<endl;
+	cout << " =>>Luas Yang Akan Dihitung A atau B : "; cin>>pilih;
+	
+	
+	
 	}
+	
