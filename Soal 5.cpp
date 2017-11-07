635d0193607320b7807004b4f816b710da85720e
Rinafahridawati <Rinafahridawti@yahoo.co.id>
2017-11-07 21:59:08 +0800

    Menginput berat badan dan tinggi badan

--- Soal 5.cpp
+++ Soal 5.cpp
@@ -6,6 +6,13 @@ int main() {
 
     float bb;
     int tb;	
+    
+    cout<<"|| PROGRAM MENENTUKAN UNDERWEIGHT, NORMAL, atau OVERWEIGHT ||"<<endl;
+   
+cout << "Silahkan masukkan tinggi badan(cm) : "; 
+cin >>tb;
+cout << "Silahkan masukkan berat badan(kg) : " ;
+cin >> bb; 
 }
 
 
